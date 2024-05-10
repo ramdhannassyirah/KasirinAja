26785bc4bdeff15add318ae43fa374d6b9c03a50
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2024-05-10 11:41:46 +0700

    UI Kasirin Aja

---
+++ resources/views/admin/user/list.blade.php
@@ -0,0 +1,49 @@
+@extends('layout.app')
+@section('content')
+<div class="content-body">
+    <div class="row page-titles mx-0">
+        <div class="col p-md-0">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
+                <li class="breadcrumb-item active"><a href="javascript:void(0)">Home</a></li>
+            </ol>
+        </div>
+    </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Data Table</h4>
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered zero-configuration">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Role</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Admin</td>
+                                    <td>admin@gmail.com</td>
+                                    <td>admin</td>
+                                    <td>
+                                        <a href="" class="btn btn-primary btn-sm">Edit <i class="fa fa-edit"></i></a>
+                                        <a href="" class="btn btn-danger btn-sm">Hapus <i class="fa fa-trash"></a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+ 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/data-user', function () {
+    return view('admin.user.list');
+})->name('data-user');
+
+Route::get('/data-barang', function () {
+    return view('admin.barang.barang');
+})->name('barang');
+
+Route::get('/detail-barang', function () {
+    return view('admin.jenisBarang.detail');
+})->name('detailBarang');
+
+
+
+
+
+
