6829f85009be759ab66121d757591abdb49f909b
ramdhannassyirahh <ramdhannassyirahh@gmail.com>
2025-08-27 08:10:39 +0700

    FIx

--- database/migrations/2024_05_20_034256_jenis_barang.php
+++ database/migrations/2024_05_20_034256_jenis_barang.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('jenis_barang', function (Blueprint $table) {
             $table->id();
-            $table->string('jenis');
+            $table->string('nama_jenis');
             $table->timestamps();
         });
     }
